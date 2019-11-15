4526566159c1e969548a8000205265bbd5ed8b40
Salavat Shamsutdinov <shamsutdinov.salavat97@gmail.com>
2019-11-15 19:36:19 +0300

    add WordsApi test

--- src/test/java/ru/kpfu/telegrambot/dictionarybot/WordsApiTest.java
+++ src/test/java/ru/kpfu/telegrambot/dictionarybot/WordsApiTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import ru.kpfu.telegrambot.dictionarybot.dictionary.WordsApiDictionaryApi;
@@ -13,6 +14,7 @@ import ru.kpfu.telegrambot.dictionarybot.model.dictionary.DictionaryResponse;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = DictionaryBotApplication.class)
+@SpringBootTest(args = {"--wordsapi.key=1234"})
 public class WordsApiTest {
 
 	@Autowired
@@ -24,5 +26,10 @@ public class WordsApiTest {
 		DictionaryResponse response = api.getResponseWithDefinition("start");
 
 		Assert.assertEquals(response.getDefinition(), "get off the ground");
+	}
+
+	@Test(expected = WordNotFoundException.class)
+	public void shouldThrowWordNotFoundException() throws WordNotFoundException {
+		api.getResponseWithDefinition("asdfasdfq");
 	}
 }
